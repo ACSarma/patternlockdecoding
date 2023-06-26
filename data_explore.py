51bd1739aee9297da5b574d409574bfc7bcc65a6
ACSarma <arun.c.sarma@gmail.com>
2023-06-25 20:55:41 -0700

    more model and data explore script

---
+++ data_explore.py
@@ -0,0 +1,61 @@
+import itertools
+import matplotlib.pyplot as plt
+import os
+import random
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import normalize, StandardScaler
+
+directory = "Data/Sync"
+type_dict = {
+    "Simple": [
+        1,
+        5
+    ],
+    "Medium_Complex": [
+        42,
+        70
+    ],
+    "Complex": [
+        108,
+        119
+    ]
+}
+resolution = 1200
+
+if __name__ == '__main__':
+    countlab = 0
+    for difficulty in type_dict.keys():
+        for pattern in type_dict[difficulty]:
+            X_data = []
+            total_data = os.listdir(f'{directory}/{difficulty}{pattern}')
+            total_data.reverse()
+            for file in total_data:
+                try:
+                    data = pd.read_csv(f'{directory}/{difficulty}{pattern}/{file}')
+                    emg = data.emg_signal
+                    emg = normalize([emg])[0]
+                    emg = np.resize(emg, resolution)
+                    emg = np.asarray(emg)
+                    X_data.append(emg)
+                except Exception as e:
+                    continue
+            objects = StandardScaler()
+            X_data = objects.fit_transform(X_data)
+            X_dataR = []
+            for emg in X_data:
+                emg = np.resize(emg, (1, resolution))
+                X_dataR.append(emg)
+            X_data = np.stack(X_dataR, axis=0)
+
+            for i in range(1):
+                rEMG = random.choice(X_data)[0]
+                plt.plot(rEMG)
+            plt.title(f'Pattern Lock Type{countlab}')
+            ticks = [0, 200, 400, 600, 800, 1000, 1200]
+            ticklabels = [0, 1000, 2000, 3000, 4000, 5000, 6000]
+            plt.xticks(ticks, ticklabels)
+            plt.xlabel('Time (ms)')
+            plt.ylabel('Amplitude')
+            plt.show()
+            countlab = countlab + 1
--- model_explor.py
+++ model_explor.py
@@ -1,24 +1,25 @@
 import datetime
-
-import serial
-import keyboard
-from time import time
-import pandas as pd
-import numpy as np
 import os
-import matplotlib.pyplot as plt
-from sklearn import metrics
-from sklearn.preprocessing import normalize
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split, StratifiedKFold, KFold
 
-from tensorflow import keras
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
 from keras.callbacks import EarlyStopping
 from keras.layers import Dense, Dropout
 from keras.layers import Flatten
 from keras.layers import LSTM
+from keras.layers import Conv1D
 from keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import normalize, StandardScaler
+from sklearn.utils import shuffle
+from keras.optimizers import Adam
+
+print("Num GPUs Available", len(tf.config.experimental.list_physical_devices('GPU')))
 
 directory = "Data/Sync"
 type_dict = {
@@ -35,17 +36,49 @@ type_dict = {
         119
     ]
 }
+resolution = 1296
+
+
+def plot_confusion_matrix(cm, classes,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    plt.title(title)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print("Normalized confusion matrix")
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    print(cm)
+    plt.colorbar()
+    plt.clim(float(0.00), float(1.00))
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, '%.2f' % (cm[i, j]),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 
 def create_model(lr, dr):
     regressor = Sequential()
-    regressor.add(LSTM(units=512, return_sequences=True, input_shape=(1, 1296)))
-    regressor.add(LSTM(units=256, return_sequences=True))
-    regressor.add(LSTM(units=128, return_sequences=True))
+    regressor.add(LSTM(units=128, return_sequences=True, input_shape=(1, resolution)))
+    regressor.add(Dropout(dr))
     regressor.add(LSTM(units=64, return_sequences=True))
     regressor.add(Dropout(dr))
+    # regressor.add(Dropout(dr))
+    # regressor.add(LSTM(units=128, return_sequences=True))
+    # regressor.add(Dropout(dr))
+    # regressor.add(LSTM(units=64, return_sequences=True))
+    # regressor.add(Dropout(dr))
+    # regressor.add(LSTM(units=32, return_sequences=True))
+    regressor.add(Dropout(dr))
     regressor.add(Flatten())
-    regressor.add(Dense(units=6, activation='softmax'))
+    regressor.add(Dense(units=6, activation='sigmoid'))
 
     opt = Adam(learning_rate=lr)
     regressor.compile(
@@ -55,6 +88,35 @@ def create_model(lr, dr):
     )
 
     return regressor
+
+
+# def create_model(lr, dr):
+#     mod = Sequential()
+#     mod.add(Dense(2048))
+#     mod.add(Dense(2048))
+#     mod.add(Dense(1024))
+#     mod.add(Dense(1024))
+#     mod.add(Dropout(dr))
+#     mod.add(Dense(512))
+#     mod.add(Dense(256))
+#     mod.add(Dropout(dr))
+#     mod.add(Dense(256))
+#     mod.add(Dense(128))
+#     mod.add(Flatten())
+#     mod.add(Dense(64))
+#     mod.add(Dense(32))
+#     mod.add(Dense(16))
+#     mod.add(Dense(12, activation='sigmoid'))
+#     # tf.keras.utils.plot_model(model, to_file='saved_model/mod.png', show_shapes=True)
+#
+#     opt = tf.keras.optimizers.SGD(learning_rate=lr)
+#     mod.compile(
+#         loss='sparse_categorical_crossentropy',
+#         optimizer=opt,
+#         metrics=['sparse_categorical_accuracy'],
+#     )
+#
+#     return mod
 
 
 X_data = []
@@ -71,8 +133,8 @@ if __name__ == '__main__':
                     eeg = data.AF8
                     emg = data.emg_signal
                     emg = normalize([emg])[0]
+                    emg = np.resize(emg, (resolution))
                     emg = np.asarray(emg)
-                    emg = np.resize(emg, (1, 1296))
                     X_data.append(emg)
                     Y_labels.append(countlab)
                 except Exception as e:
@@ -83,25 +145,31 @@ if __name__ == '__main__':
     X_data = np.stack(X_data, axis=0)
     print(X_data.shape)
     print(Y_labels.shape)
+    objects = StandardScaler()
+    X_data = objects.fit_transform(X_data)
+    X_dataR = []
+    for emg in X_data:
+        emg = np.resize(emg, (1, resolution))
+        X_dataR.append(emg)
+    X_data = np.stack(X_dataR, axis=0)
 print(len(np.unique(Y_labels)))
 print("Starting Model")
 
 accuracies = []
 summary = ""
-num_train = 2
-drs = [0.05, 0.2, 0.4]  # dropout rates testing
-lrs = [0.001, 0.0001]  # learning rates testing
+drs = [0.4]  # dropout rates testing
+lrs = [0.0001]  # learning rates testing
 epochs = 200
 best_hist = None
 
-s = np.arange(0, len(X_data), 1)
-shuffle(s)
-X_data = X_data[s]
-Y_labels = Y_labels[s]
+# s = np.arange(0, len(X_data), 1)
+# shuffle(s)
+# X_data = X_data[s]
+# Y_labels = Y_labels[s]
 # GridSearch with Cross Validation
 for r in range(len(drs)):
     for c in range(len(lrs)):
-        kfolds = KFold(n_splits=2, shuffle=True, random_state=0)
+        kfolds = KFold(n_splits=5, shuffle=True, random_state=0)
         accuracies1 = []
         accuracies2 = []
         for train_mask, test_mask in kfolds.split(X_data, Y_labels):
@@ -114,7 +182,7 @@ for r in range(len(drs)):
             model2 = create_model(lrs[c], drs[r])
 
             callback = EarlyStopping(
-                monitor='sparse_categorical_accuracy', min_delta=0.0001,
+                monitor='sparse_categorical_accuracy', min_delta=0.0005,
                 patience=10)
 
             hist = model2.fit(X_trainC, y_trainC, epochs=200, callbacks=[callback])
@@ -123,11 +191,23 @@ for r in range(len(drs)):
             acc = metrics.accuracy_score(y_testC, y_predicted_labels)
             print("Accuracy on Test: ", acc)
 
+            cm = confusion_matrix(y_testC, y_predicted_labels)
+            plot_confusion_matrix(cm, classes=range(10),
+                                  title='')
+            FP = cm.sum(axis=0) - np.diag(cm)
+            FN = cm.sum(axis=1) - np.diag(cm)
+            TP = np.diag(cm)
+            TN = cm.sum() - (FP + FN + TP)
+            FNR = FN / (TP + FN)
+            TPR = TP / (TP + FN)
+
+            plt.show()
+
             accuracies2.append(acc)
             if max(accuracies2) == acc:
                 best_hist = hist
 
-            summary += f'Dropout: {drs[r]}, Learning Rate: {lrs[c]}; Accuracy: {acc} - {datetime.datetime.now()} \n'
+            summary += f'Dropout: {drs[r]}, Learning Rate: {lrs[c]}; Accuracy: {acc} - {datetime.datetime.now()}; FNR: {FNR}; TPR: {TPR} \n'
         print(accuracies2, "\nAverage Accuracy: ", np.average(accuracies2))
 
 e_list = []
