f3aaf9aabb3b7b39ebcd510f0625d36cd2d795a4
ACSarma <arun.c.sarma@gmail.com>
2023-09-06 22:37:14 -0700

    lines results initial

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import serial
+import random
 import keyboard
 from time import time
 import pandas as pd
@@ -25,19 +25,29 @@ type_dict = {
 
 count = 0
 if __name__ == '__main__':
-    for difficulty in type_dict.keys():
-        for pattern in type_dict[difficulty]:
-            total_data = os.listdir(f'{directory}/{difficulty}{pattern}')
-            total_data.reverse()
-            for file in total_data:
-                if count == 10:
-                    plt.show()
-                count += 1
-                data = pd.read_csv(f'{directory}/{difficulty}{pattern}/{file}')
-                eeg = data.AF8
-                emg = data.emg_signal
-                emg = normalize([emg])[0]
-                plt.plot(emg)
+    lines = os.listdir(f'{directory}/Lines')
+    lines.sort()
+    for li in lines:
+        emg = pd.read_csv(f'{directory}/Lines/{li}/Lines{random.randint(0, 40)}.csv')
+        emg = emg.emg_signal
+        emg = normalize([emg])[0]
+        plt.plot(emg)
+        plt.legend(["L0", "L1", "L2", "L3", "L4", "L5", "L6", "L7", "L8", "L9", "L10", "L11", "L12", "L13", "L14", "L15", "L16"])
+    plt.show()
+
+    # for difficulty in type_dict.keys():
+    #     for pattern in type_dict[difficulty]:
+    #         total_data = os.listdir(f'{directory}/{difficulty}{pattern}')
+    #         total_data.reverse()
+    #         for file in total_data:
+    #             if count == 10:
+    #                 plt.show()
+    #             count += 1
+    #             data = pd.read_csv(f'{directory}/{difficulty}{pattern}/{file}')
+    #             eeg = data.AF8
+    #             emg = data.emg_signal
+    #             emg = normalize([emg])[0]
+    #             plt.plot(emg)
     # for f in os.listdir("Data/Sync/"):
     #     if f.startswith("Complex108") and not f == "Complex108" and not f == "Complex119" and not f == "Medium_Complex70" and not f == "Medium_Complex42" and not f == "Simple5" and not f == "Simple1":
     #         df = pd.read_csv(f'Data/Sync/{f}')
